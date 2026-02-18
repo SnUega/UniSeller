9217194c85dd2c2a4dd91548faa9f9f42e845a32
SnUega <snuega@gmail.com>
2026-02-18 14:01:26 +0300

    Update base path for UniSeller repository

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/Uniseller-TEST/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/UniSeller/' : '/',
   server: {
     port: 3000,
     open: true
